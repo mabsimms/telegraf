e1570cde9e453571fd8e98462f9a6351e04cb7b5
Mark Simms <masimms@microsoft.com>
2018-03-28 22:28:48 +0000

    updated metadata parsing

--- plugins/outputs/azuremonitor/azuremetadata_test.go
+++ plugins/outputs/azuremonitor/azuremetadata_test.go
@@ -8,9 +8,11 @@ func TestGetMetadata(t *testing.T) {
 	azureMetadata := &AzureInstanceMetadata{}
 	metadata, err := azureMetadata.GetInstanceMetadata()
 	if err != nil {
-		t.Logf("could not get metadata")
+		t.Logf("could not get metadata: %v\n", err)
+	} else { 
+		t.Logf("resource id  \n%s", metadata.AzureResourceID)
+		t.Logf("metadata is \n%v", metadata)
 	}
 
 	//fmt.Printf("metadata is \n%v", metadata)
-	t.Logf("raw metadata is \n%v", metadata.Raw)
 }
